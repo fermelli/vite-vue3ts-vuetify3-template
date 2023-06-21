d89e1bfc140cab06f26e446580cf4293ba8bb697
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-06-21 01:11:48 -0400

    consumir servicio

---
+++ src/modules/pages/products/components/ProductTable.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+import { ProductInterface } from "@/interfaces/product.interface";
+
+defineProps({
+  products: {
+    type: Array as PropType<ProductInterface[]>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <v-table density="compact">
+    <thead>
+      <tr>
+        <th class="text-left">ID.</th>
+        <th class="text-left">Name</th>
+        <th class="text-left">Description</th>
+        <th class="text-right">Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="product in products"
+        :key="product.name"
+      >
+        <td>{{ product.id }}</td>
+        <td>{{ product.name }}</td>
+        <td>{{ product.description }}</td>
+        <td class="text-right">{{ product.price }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
--- src/modules/pages/products/views/ProductsView.vue
+++ src/modules/pages/products/views/ProductsView.vue
@@ -1,7 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { productsService } from "@/services";
+import { onMounted, ref } from "vue";
+import { ProductInterface } from "@/interfaces/product.interface";
+import ProductTable from "../components/ProductTable.vue";
+
+const products = ref<ProductInterface[]>([]);
+const loading = ref<boolean>(false);
+
+onMounted(async () => {
+  loading.value = true;
+  try {
+    const response = await productsService.get();
+
+    products.value = response.data;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
 
 <template>
   <div>
     <h1>ProductosView</h1>
+
+    <div
+      v-if="products.length === 0"
+      class="d-flex justify-center items-center py-6"
+    >
+      <v-progress-circular
+        model-value="20"
+        :size="40"
+        :indeterminate="loading"
+      ></v-progress-circular>
+    </div>
+
+    <product-table
+      v-else
+      :products="products"
+    ></product-table>
   </div>
 </template>
