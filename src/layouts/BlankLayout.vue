8d9fbb0ab8e090f35d3a80adf7a50746d1e41178
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-09-02 22:17:59 -0400

    agregado blank layout

---
+++ src/layouts/BlankLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-layout>
+    <v-main>
+      <v-container fluid>
+        <slot></slot>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
