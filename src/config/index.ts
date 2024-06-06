dfeb0e9c23788ad4583ef791711f7e89a39e5eba
fermelli <luisfernandosalgadomiguez@gmail.com>
2024-06-06 00:27:41 -0400

    actualizar archivo de configuracion de red para eliminar codigos de estado
    se emplearan los codigos de estado del enum HttpStatusCode de axios

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,24 +1,3 @@
 import networkConfig from "./network.config";
 
-export const {
-  baseURL,
-  contentType,
-  accept,
-  errorNetworkCode,
-  messageDuration,
-  timeout,
-  okCode,
-  createdCode,
-  notContentCode,
-  movedPermanentlyCode,
-  badRequestCode,
-  unauthorizedCode,
-  forbiddenCode,
-  notFoundCode,
-  methodNotAllowCode,
-  requestTimeoutCode,
-  unprocessableEntityCode,
-  tooManyRequestsCode,
-  internalServerCode,
-  serviceUnavailableCode,
-} = networkConfig;
+export const { baseURL, contentType, accept, errorNetworkCode, messageDuration, timeout } = networkConfig;
